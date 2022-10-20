6e26a969c47983811c0d1b6581515950e8ad1c8f
nelkor <a.tutunnik@corp.mail.ru>
2022-10-20 10:24:11 +0600

    icons to <use> wip

--- src/modules/ui/icons/CheckMarkIcon.vue
+++ src/modules/ui/icons/CheckMarkIcon.vue
@@ -3,19 +3,8 @@
     width="8"
     height="8"
     viewBox="0 0 8 8"
-    fill="white"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <g clip-path="url(#clip0_6_12)">
-      <path
-        d="M7.95452 2.1527L3.24992 7.44537L0 4.19545L1.41421 2.78123L3.16429 4.53131L6.4597 0.823975L7.95452 2.1527Z"
-      />
-    </g>
-
-    <defs>
-      <clipPath id="clip0_6_12">
-        <rect width="8" height="8" />
-      </clipPath>
-    </defs>
+    <use href="#check-mark" />
   </svg>
 </template>
